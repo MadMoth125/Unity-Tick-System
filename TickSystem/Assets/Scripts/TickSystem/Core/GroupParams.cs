b0340c4715629d0f6008ebc09a4843b55ab4d104
MadMoth125 <madmoth125@gmail.com>
2024-05-27 02:44:45 -0500

    Added struct for group parameters
    A struct containing only information about a tick group.
    Attempting to future-proof by having a collection of info rather than sending individual strings and bools.

---
+++ TickSystem/Assets/Scripts/TickSystem/Core/GroupParams.cs
@@ -0,0 +1,35 @@
+
+namespace TickSystem.Core
+{
+	public struct GroupParams
+	{
+		public string name;
+		public int tickRate;
+		public bool active;
+		public bool useRealTime;
+		
+		public GroupParams(string name, int tickRate, bool active, bool useRealTime)
+		{
+			this.name = name;
+			this.tickRate = tickRate;
+			this.active = active;
+			this.useRealTime = useRealTime;
+		}
+		
+		public GroupParams(string name, int tickRate, bool active)
+		{
+			this.name = name;
+			this.tickRate = tickRate;
+			this.active = active;
+			this.useRealTime = false;
+		}
+		
+		public GroupParams(string name, int tickRate)
+		{
+			this.name = name;
+			this.tickRate = tickRate;
+			this.active = true;
+			this.useRealTime = false;
+		}
+	}
+}

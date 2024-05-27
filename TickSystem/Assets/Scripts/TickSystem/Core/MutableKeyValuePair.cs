e6ccc6e5cc3c4000ad4772f3f080186f78ed41ca
MadMoth125 <madmoth125@gmail.com>
2024-05-27 05:49:53 -0500

    Created mutable version of a KVP
    Somewhat overly robust, but co-pilot did most of the work.

---
+++ TickSystem/Assets/Scripts/TickSystem/Core/MutableKeyValuePair.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+namespace TickSystem.Core
+{
+	public struct MutableKeyValuePair<TKey, TValue>
+	{
+		public TKey Key { get; set; }
+		public TValue Value { get; set; }
+		
+		public MutableKeyValuePair(TKey key, TValue value)
+		{
+			Key = key;
+			Value = value;
+		}
+
+		public MutableKeyValuePair(KeyValuePair<TKey, TValue> pair)
+		{
+			Key = pair.Key;
+			Value = pair.Value;
+		}
+		
+		public void CopyFrom(MutableKeyValuePair<TKey, TValue> other)
+		{
+			Key = other.Key;
+			Value = other.Value;
+		}
+		
+		public void CopyFrom(KeyValuePair<TKey, TValue> pair)
+		{
+			Key = pair.Key;
+			Value = pair.Value;
+		}
+		
+		public KeyValuePair<TKey, TValue> AsKeyValuePair()
+		{
+			return new KeyValuePair<TKey, TValue>(Key, Value);
+		}
+		
+		public override string ToString()
+		{
+			return $"[{Key}, {Value}]";
+		}
+		
+		public override bool Equals(object obj)
+		{
+			if (obj is MutableKeyValuePair<TKey, TValue> other)
+			{
+				return Key.Equals(other.Key) && Value.Equals(other.Value);
+			}
+			return false;
+		}
+
+		public bool Equals(MutableKeyValuePair<TKey, TValue> other)
+		{
+			return EqualityComparer<TKey>.Default.Equals(Key, other.Key) && EqualityComparer<TValue>.Default.Equals(Value, other.Value);
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Key, Value);
+		}
+
+		public static implicit operator MutableKeyValuePair<TKey, TValue>(KeyValuePair<TKey, TValue> pair)
+		{
+			return new MutableKeyValuePair<TKey, TValue>(pair);
+		}
+		
+		public static implicit operator KeyValuePair<TKey, TValue>(MutableKeyValuePair<TKey, TValue> pair)
+		{
+			return new KeyValuePair<TKey, TValue>(pair.Key, pair.Value);
+		}
+	}
+}

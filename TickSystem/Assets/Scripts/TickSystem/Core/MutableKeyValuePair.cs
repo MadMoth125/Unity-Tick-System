4af9479fae2dee6dc25b60a7dbaa6fc3f4cc9847
MadMoth125 <madmoth125@gmail.com>
2024-05-27 06:18:30 -0500

    Update MutableKeyValuePair.cs
    Changed from a struct to a class because it seems to be way more convenient for my use-case.

--- TickSystem/Assets/Scripts/TickSystem/Core/MutableKeyValuePair.cs
+++ TickSystem/Assets/Scripts/TickSystem/Core/MutableKeyValuePair.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 
 namespace TickSystem.Core
 {
-	public struct MutableKeyValuePair<TKey, TValue>
+	public class MutableKeyValuePair<TKey, TValue>
 	{
-		public TKey Key { get; private set; }
-		public TValue Value { get; private set; }
+		public TKey Key;
+		public TValue Value;
 		
 		public MutableKeyValuePair(TKey key, TValue value)
 		{
@@ -37,16 +37,6 @@ namespace TickSystem.Core
 			Key = key;
 			Value = value;
 		}
-
-		public void SetKey(TKey key)
-		{
-			Key = key;
-		}
-
-		public void SetValue(TValue value)
-		{
-			Value = value;
-		}
 		
 		public void Deconstruct(out TKey key, out TValue value)
 		{
@@ -62,25 +52,6 @@ namespace TickSystem.Core
 		public override string ToString()
 		{
 			return $"[{Key}, {Value}]";
-		}
-		
-		public override bool Equals(object obj)
-		{
-			if (obj is MutableKeyValuePair<TKey, TValue> other)
-			{
-				return Key.Equals(other.Key) && Value.Equals(other.Value);
-			}
-			return false;
-		}
-
-		public bool Equals(MutableKeyValuePair<TKey, TValue> other)
-		{
-			return EqualityComparer<TKey>.Default.Equals(Key, other.Key) && EqualityComparer<TValue>.Default.Equals(Value, other.Value);
-		}
-
-		public override int GetHashCode()
-		{
-			return HashCode.Combine(Key, Value);
 		}
 
 		public static implicit operator MutableKeyValuePair<TKey, TValue>(KeyValuePair<TKey, TValue> pair)
